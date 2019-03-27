2e1aa942721ecd6a1f5f518e6fa07ae74eb33f13
RookieDeveloper <luorongsheng@richinfo.cn>
2019-03-27 14:19:11 +0800

    管理端

--- blog-client/src/router/PerMission.jsx
+++ blog-client/src/router/PerMission.jsx
@@ -8,8 +8,13 @@ export default class PerMission extends React.Component {
         const { pathname } = location
         const isLogin = localStorage.getItem('__config_center_token')
         let component1
-        if(!isLogin) { // 判断是否登录
-            return <Redirect to="/login"></Redirect>
+        if(!isLogin) { // 判断是否登录,isLogin不存在
+            if (pathname === '/login') {
+                component1 = config.getCurrntRoute(pathname, config.routerConfig)
+                return <Route exact path='/login' component={component1.component}></Route>
+            }else{
+                return <Redirect to="/login"></Redirect>
+            }
         }else {
             if (pathname === '/login') {
                 return <Redirect to="/"></Redirect>
