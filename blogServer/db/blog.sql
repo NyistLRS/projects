6bd4b2cb674e8c4a160154bf6848fb2f315322fe
RookieDeveloper <luorongsheng@richinfo.cn>
2019-04-04 15:32:47 +0800

    更新sql

--- blogServer/db/blog.sql
+++ blogServer/db/blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2019-04-01 17:25:18
+Date: 2019-04-04 15:32:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,19 +23,20 @@ CREATE TABLE `admin_menu` (
   `icon` varchar(255) DEFAULT NULL,
   `title` varchar(256) DEFAULT NULL,
   `pathname` varchar(256) DEFAULT NULL,
-  `id` int(11) NOT NULL,
-  `leavel` int(11) NOT NULL,
-  `parentId` int(11) DEFAULT NULL
+  `id` varchar(256) NOT NULL,
+  `leavel` int(11) DEFAULT '0',
+  `parentId` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of admin_menu
 -- ----------------------------
-INSERT INTO `admin_menu` VALUES ('home', '首页', '/', '1', '0', null);
-INSERT INTO `admin_menu` VALUES ('appstore', '菜单管理', '/menu', '2', '0', null);
-INSERT INTO `admin_menu` VALUES ('file', '用户管理', '/user', '3', '0', null);
-INSERT INTO `admin_menu` VALUES ('file', '系统设置', '/setting', '4', '0', null);
-INSERT INTO `admin_menu` VALUES ('file', '权限设置', '/permission', '5', '1', '4');
+INSERT INTO `admin_menu` VALUES ('home', '首页', '/', '1554339479033186', '0', null);
+INSERT INTO `admin_menu` VALUES ('appstore', '菜单管理', '/menu', '1554339531500067', '0', null);
+INSERT INTO `admin_menu` VALUES ('file', '用户管理', '/user', '1554339589919753', '0', null);
+INSERT INTO `admin_menu` VALUES ('file', '系统设置', '/setting', '1554344922048205', '0', null);
+INSERT INTO `admin_menu` VALUES ('file', '权限设置', '/permission', '1554345015610314', '0', '1554344922048205');
 
 -- ----------------------------
 -- Table structure for user
